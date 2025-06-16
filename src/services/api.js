89db54e3ae1256d1312a1b3de3240e683fdcce4d
Abhinav Kumar <abhinavkum03@gmail.com>
2025-06-16 23:02:50 +0530

    Initial commit

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "e2911bb1a194788d7fc1aafd2d0edd3b";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json(); // <- await is necessary
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json(); // <- await is necessary
+  return data.results;
+};
